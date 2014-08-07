82393b7a2987b7e85a49882f0badbeb3460ffea0
John Manuel <bitencode@johnmanuel.org>
2014-08-07 16:33:41 -0700

    Support Slim plugins via slim: { plugins: 'slim/plugin_name'} option. Verbose option via verbose: true/false to show more information about what guard-slim is doing. Display a stack trace if we rescue a StandardError.

--- lib/guard/slim.rb
+++ lib/guard/slim.rb
@@ -9,21 +9,23 @@ module Guard
     ALL      = File.join '**', '*'
     Template = ::Slim::Template
 
-    attr_reader :input, :output, :context, :slim
+    attr_reader :input, :output, :context, :slim_opts, :slim_plugins
 
     def initialize(options = {})
-      ::Guard::UI.deprecation(":input_root has been replaced with :input") if @input = options.delete(:input_root)
-      ::Guard::UI.deprecation(":output_root has been replaced with :output") if @output = options.delete(:output_root)
-      @input    ||= options.delete(:input)   { Dir.getwd }
-      @output   ||= options.delete(:output)  { Dir.getwd }
-      @context  = options.delete(:context) { Object.new }
-      @slim     = options.delete(:slim)    { Hash.new }
+      ::Guard::UI.warning(":input_root has been replaced with :input") if @input = options.delete(:input_root)
+      ::Guard::UI.warning(":output_root has been replaced with :output") if @output = options.delete(:output_root)
+      @input     ||= options.delete(:input) { Dir.getwd }
+      @output    ||= options.delete(:output) { Dir.getwd }
+      @context   = options.delete(:context) { Object.new }
+      @slim_opts = Hash(options.delete(:slim))
+      @slim_plugins = Array(slim_opts.delete(:plugins))
 
       super
     end
 
 
     def start
+      slim_plugins.each {|p| context.send(:require, p)}
       if options[:compile_on_start]
         UI.info 'Guard-Slim: is going to compile your markup.'
         run_all
@@ -40,30 +42,31 @@ module Guard
 
 
     def reload
-      ::Guard::UI.info "Guard-Slim: Reload."
+      ::Guard::UI.info "Guard-Slim: Reload." if options[:verbose]
       true
     end
 
 
     def run_all
-      ::Guard::UI.info "Guard-Slim: Run All."
+      ::Guard::UI.info "Guard-Slim: Run All." if options[:verbose]
       run_on_changes all_paths
     end
 
 
     def run_on_changes(paths)
-      ::Guard::UI.info "Guard-Slim: Run On Changes (#{paths.inspect})."
+      ::Guard::UI.info "Guard-Slim: Run On Changes (#{paths.inspect})." if options[:verbose]
       paths.each do |path|
         begin
           content = render File.read(path)
           open(build_path(path), 'w') do |file|
-            slim[:pretty] ?
+            slim_opts[:pretty] ?
               file.puts(content) :
               file.write(content)
           end
           ::Guard::UI.info "Guard-Slim: Rendered #{ path } to #{ build_path path }"
-        rescue StandardError => error
-          ::Guard::UI.info "Slim Error: " + error.message
+        rescue StandardError => ex
+          ::Guard::UI.info "Slim Error: #{ex.message}"
+          print ex.backtrace * "\n"
         end
       end
       true
@@ -76,7 +79,7 @@ module Guard
 
 
     def run_on_removals(paths)
-      ::Guard::UI.info "Guard-Slim: Removal - not implemented yet."
+      ::Guard::UI.info "Guard-Slim: Removal - not implemented yet. Paths = #{paths.inspect}"
       true
     end
 
@@ -96,11 +99,11 @@ module Guard
 
 
       def render(source)
-        Template.new(@slim) { source }.render(context)
+        Template.new(slim_opts) { source }.render(context)
       rescue SyntaxError => ex
         ::Guard::UI.info se.message
       rescue Exception => ex
-        ::Guard::UI.info e.message
+        ::Guard::UI.info ex.message
       end
 
 
